454147411f84468d71574dfa9a30c4892ca73945
seohyunCho <shanny06@naver.com>
2023-11-13 10:52:15 +0900

    Noto Sans 폰트 추가

--- src/main/java/org/example/SettingClass.java
+++ src/main/java/org/example/SettingClass.java
@@ -3,6 +3,9 @@ package org.example;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -24,5 +27,20 @@ public class SettingClass {
         result.close();
         statement.close();
         connection.close();
+    }
+    public static void customFont(JComponent component, int fontStyle, int fontSize) {
+        String fontPath = "NotoSansKR-Medium.ttf"; // 다운로드한 파일명에 맞게 수정
+        Font notoSansFont;
+        // 폰트 로딩
+        try {
+            notoSansFont = Font.createFont(Font.TRUETYPE_FONT, new File(fontPath)).deriveFont(fontStyle, fontSize);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(notoSansFont);
+
+            component.setFont(notoSansFont);
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+            // 폰트 로딩에 실패한 경우 예외 처리
+        }
     }
 }
